70b2715716747a5ec61f18f165265cdae5215e75
Martin <msmith3dart@icloud.com>
2020-02-05 17:52:37 -0800

    update

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,9 +5,9 @@ def canUnlockAll(boxes):
     if not isinstance(boxes, list):
         return False
     n_boxes = len(boxes)
-    if n_boxes is 1 or n_boxes is 0:
+    if n_boxes == 1 or n_boxes == 0:
         return True
-    elif n_boxes is 0 or len(boxes[0]) is 0:
+    elif n_boxes == 0 or len(boxes[0]) == 0:
         return False
     key_ring = boxes[0]
     visited = {k: False for k in range(n_boxes)}
@@ -22,18 +22,20 @@ def canUnlockAll(boxes):
 
 def unlock(keys: list, boxes: list, visited: dict, n_boxes: int):
     for k in keys:
-        # print(keys)
-        if k > n_boxes - 1 or visited[k]:
-            keys.remove(k)
-        else:
-            b = list()
-            keys.remove(k)
-            visited[k] = True
-            try:
-                b = [i for i in boxes[k] if not visited[i]]
-            except ValueError:
-                pass
-            keys += b
+        try:
+            if k > n_boxes - 1 or visited[k]:
+                keys.remove(k)
+            else:
+                b = list()
+                keys.remove(k)
+                visited[k] = True
+                try:
+                    b = [i for i in boxes[k] if not visited[i]]
+                except ValueError:
+                    pass
+                keys += b
+        except Exception:
+            pass
 
     return keys if len(keys) > 0 else None
 
@@ -44,8 +46,8 @@ def r_unlock(keys: list, boxes: list, visited: dict, n_boxes: int):
     k, *keys = keys
     try:
         if k > n_boxes - 1 or visited[k]:
+            keys = [k for k in keys if k < n_boxes]  # remove redundent keys.
             r_unlock(keys, boxes, visited, n_boxes)
-            # keys = [i for i in keys if i < n_boxes or not visited[i]]
         else:
             visited[k] = True
             b = list()
