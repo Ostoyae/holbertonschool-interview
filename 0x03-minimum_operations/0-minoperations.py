2574d88e5c975740568746c03fc40f19d9fcb8c7
Martin <msmith3dart@icloud.com>
2020-02-25 13:26:28 -0800

    dropped hard codes primes
    using a loop

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,33 +6,55 @@ needed to result in exactly n H characters in the file.
 """
 
 
+def _minOperations(n):
+
+    f = 0
+
+    if n >= 2:
+        while n > 1:
+            if n % 7 == 0:
+                f += 7
+                n //= 7
+            elif n % 5 == 0:
+                f += 5
+                n //= 5
+            elif n % 3 == 0:
+                f += 3
+                n //= 3
+            elif n % 2 == 0:
+                f += 2
+                n //= 2
+            else:
+                f += n
+                n //= n
+
+    return int(f)
+
+
 def minOperations(n):
     """calculates the fewest number of operations
 
     Keyword arguments:
     n -- number of Charaters to achieve
     """
+
+    if n in [0, 1]:
+        return 0
+
     f = 0
+    while n % 2 == 0:
+        n //= 2
+        f += 2
 
-    if n >= 2:
-        while n > 1:
-            if n % 2 == 0:
-                f += 2
-                n //= 2
-            elif n % 3 == 0:
-                f += 3
-                n //= 3
-            elif n % 5 == 0:
-                f += 5
-                n //= 5
-            elif n % 7 == 0:
-                f += 7
-                n //= 7
-            else:
-                f += n
-                n //= n
+    i = 2
+    while i <= n:
+        if n % i:
+            i += 1
+        else:
+            f += i
+            n //= i
 
-    return int(f)
+    return f
 
 
 if __name__ == "__main__":
