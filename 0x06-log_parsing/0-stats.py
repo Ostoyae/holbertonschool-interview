5694689f2fd1a5c6d8c3eb1f1a0fec86c9d801eb
Martin <msmith3dart@icloud.com>
2020-03-16 13:51:52 -0700

    add project log parse

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+import cmd
+import sys
+import signal
+
+
+class LineParser:
+
+    @staticmethod
+    def parse_line(line):
+        data_slice = line.split(" ")[7:9]
+        return (int(data_slice[0]), int(data_slice[1]))
+
+
+class StateCode:
+    _buf_size = 512
+    _file_size = 0
+    _read_iter = 0
+    _status = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+    def process(self, line):
+        self._read_iter += 1
+        (status, length) = LineParser.parse_line(line)
+        self._file_size += length
+        self._status[status] += 1
+
+        if self._read_iter % 10 == 0:
+            print(f"File size: {self._file_size}")
+            for (k, v) in self._status.items():
+                if v > 0:
+                    print(f"{k}: {v}")
+
+    def loop(self):
+        def signal_handel(sig, frame):
+            sc = ""
+            for (k, v) in self._status.items():
+                if v > 0:
+                    sc += f"{k}: {v}\n"
+
+            print(f"File size: {self._file_size}\n{sc}", sep="")
+
+            raise KeyboardInterrupt
+
+        signal.signal(signal.SIGINT, signal_handel)
+        with sys.stdin as reader:
+            while True:
+                line = reader.readline(self._buf_size)[1:]
+                if (len(line) > 0):
+                    self.process(line)
+
+
+if __name__ == "__main__":
+    StateCode().loop()
