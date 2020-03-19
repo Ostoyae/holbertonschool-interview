5f2f799411672acf86fb769b54150d0db0264fa7
martin smith <mcodesmith@gmail.com>
2020-03-19 14:09:40 -0700

    just trying to please the Holbertons code checker

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ 0x06 log parsing This program will parse each line."""
 
-import signal
 import sys
 
 
@@ -23,62 +22,33 @@ class LineParser:
         return (0, 0)
 
 
-class StateCode:
-    """ Define class for reading lines form stdin
-    """
+if __name__ == "__main__":
 
-    _buf_size = 512
-    _file_size = 0
-    _read_iter = 0
+    buf_size = 512
+    file_size = 0
+    read_iter = 0
     _status = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-    def process(self, line):
-        """handles each `line`
-        """
+    with sys.stdin as reader:
+        try:
+            while True:
+                line = reader.readline(buf_size)[1:]
+                if len(line) > 0:
+                    read_iter += 1
+                    (status, length) = LineParser.parse_line(line)
+                    file_size += length
+                    if status in _status.keys():
+                        _status[status] += 1
 
-        self._read_iter += 1
-        (status, length) = LineParser.parse_line(line)
-        self._file_size += length
-        if status in self._status.keys():
-            self._status[status] += 1
-
-        if self._read_iter % 10 == 0:
-            print("File size: {}".format(self._file_size))
-            for (k, v) in self._status.items():
+                    if read_iter % 10 == 0:
+                        print("File size: {}".format(file_size))
+                        for (k, v) in _status.items():
+                            if v > 0:
+                                print("{}: {}".format(k, v))
+        except KeyboardInterrupt:
+            sc = ""
+            for (k, v) in _status.items():
                 if v > 0:
-                    print("{}: {}".format(k, v))
+                    sc += "{key}: {val}\n".format(key=k, val=v)
 
-    def loop(self):
-        """
-        looping body of the application
-        """
-
-        # def signal_handel(sig, frame):
-        #     sc = ""
-        #     for (k, v) in self._status.items():
-        #         if v > 0:
-        #             sc += "{key}: {val}\n".format(key=k, val=v)
-        #
-        #     print("File size: {}\n{}".format(self._file_size, sc, sep=""))
-        #
-        #     raise KeyboardInterrupt
-        #
-        # signal.signal(signal.SIGINT, signal_handel)
-        with sys.stdin as reader:
-            try:
-                while True:
-                    line = reader.readline(self._buf_size)[1:]
-                    if len(line) > 0:
-                        self.process(line)
-            except KeyboardInterrupt:
-                sc = ""
-                for (k, v) in self._status.items():
-                    if v > 0:
-                        sc += "{key}: {val}\n".format(key=k, val=v)
-
-                print("File size: {}\n{}".format(self._file_size, sc, sep=""))
-
-
-if __name__ == "__main__":
-    """Entry point"""
-    StateCode().loop()
+            print("File size: {}\n{}".format(file_size, sc, sep=""))
