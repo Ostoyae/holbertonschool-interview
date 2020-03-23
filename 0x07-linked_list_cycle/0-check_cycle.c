ce7eaa786c0971e962c30e22a7fe55fbccbcec5d
Martin Smith <mcodesmith@gmail.com>
2020-03-23 13:02:27 -0700

    minor condition change

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -7,26 +7,26 @@
  */
 int check_cycle(listint_t *list)
 {
-listint_t *slow, *fast;
-int toggle;
+	listint_t *slow, *fast;
+	int toggle;
 
-if (!list)
-	return (1);
-else if (!list->next)
+	if (!list)
+		return (0);
+	else if (!list->next)
+		return (0);
+
+	toggle = 0;
+	slow = fast = list;
+
+	while (fast)
+	{
+		toggle++;
+		fast = fast->next;
+		if (slow == fast)
+			return (1);
+		if (toggle % 2)
+			slow = slow->next;
+	}
+
 	return (0);
-
-toggle = 0;
-slow = fast = list;
-
-while (fast)
-{
-toggle++;
-fast = fast->next;
-if (slow == fast)
-return (1);
-if (toggle % 2)
-slow = slow->next;
-}
-
-return (0);
 }
