6e38f011adea34b83f42f9c244906f7241bb083c
Martin Smith <mcodesmith@gmail.com>
2020-04-10 18:14:58 -0700

    fix slicing of data

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -30,31 +30,32 @@ def validUTF8(data):
                 i += 1
                 continue
             if data[i] & (1 << 6) == 0:
-                if (not validUTF8_bytes(data[i:i+1])):
-                    return False
-                i += 1
-            elif data[i] & (1 << 5) == 0:
                 if (not validUTF8_bytes(data[i:i+2])):
                     return False
                 i += 2
-            elif data[i] & (1 << 4) == 0:
+            elif data[i] & (1 << 5) == 0:
                 if (not validUTF8_bytes(data[i:i+3])):
                     return False
                 i += 3
+            elif data[i] & (1 << 4) == 0:
+                if (not validUTF8_bytes(data[i:i+4])):
+                    return False
+                i += 4
             else:
                 return False
         except IndexError:
             return False
 
-        i += 1
 
     return True
 
 
 def validUTF8_bytes(data):
     """ method that will validate a set of bytes as utf-8"""
-    for b in data[1:]:
-        if b & (1 << 7) != 1:
+    
+
+    for b in data:
+        if b & (1 << 7) != 0:
             return False
 
     return True
