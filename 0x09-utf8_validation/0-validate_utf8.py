6784fc9fb6c040f8708bbd6929a4f5bfc5be0ab4
Martin Smith <mcodesmith@gmail.com>
2020-04-10 18:23:18 -0700

    fix bitwise logic

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -26,18 +26,18 @@ def validUTF8(data):
     i = 0
     while i < len(data):
         try:
-            if data[i] & (1 << 7) == 0:
+            if data[i] & (1 << 8) == 0:
                 i += 1
                 continue
-            if data[i] & (1 << 6) == 0:
+            if data[i] & (1 << 7) == 0:
                 if (not validUTF8_bytes(data[i:i+2])):
                     return False
                 i += 2
-            elif data[i] & (1 << 5) == 0:
+            elif data[i] & (1 << 6) == 0:
                 if (not validUTF8_bytes(data[i:i+3])):
                     return False
                 i += 3
-            elif data[i] & (1 << 4) == 0:
+            elif data[i] & (1 << 5) == 0:
                 if (not validUTF8_bytes(data[i:i+4])):
                     return False
                 i += 4
@@ -54,7 +54,7 @@ def validUTF8_bytes(data):
     """ method that will validate a set of bytes as utf-8"""
     
 
-    for b in data:
+    for b in data[1:]:
         if b & (1 << 7) != 0:
             return False
 
