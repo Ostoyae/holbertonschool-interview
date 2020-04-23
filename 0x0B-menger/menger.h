197b84e2c299da96c929ff7d07faba4e7c0accba
Martin Smith <mcodesmith@gmail.com>
2020-04-22 22:42:54 -0700

    added something for 0x0B

---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,10 @@
+#ifndef INTERVIEW_MENGER_H
+#define INTERVIEW_MENGER_H
+
+#include <stdio.h>
+
+void menger(int level);
+char **generate_sponge(int);
+void print_sponge(char**, int level);
+
+#endif
