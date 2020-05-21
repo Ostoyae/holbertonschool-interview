a8c1f238401add1430148e0f1f5797db1978cae8
Martin Smith <mcodesmith@gmail.com>
2020-05-20 23:48:00 -0700

    adding stuff

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include "binary_trees.h"
+#include <stdlib.h>
+
+
+static avl_t *new_node(int n, avl_t *parent);
+
+void from_avl_lib()
+{
+	printf("from array to avl lib");
+}
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	avl_t *head;
+	size_t h_idx;
+
+	if (!array || size == 0)
+		return (NULL);
+	h_idx = size / 2;
+	if (h_idx % 2 == 0)
+		h_idx--;
+	head = new_node(array[h_idx], NULL);
+	head->left = sorted_array_to_avl_rec(
+		array, head, size, 0, h_idx - 1);
+	head->right = sorted_array_to_avl_rec(
+		array, head, size, h_idx + 1, size - 1);
+
+	return (head);
+}
+
+avl_t *sorted_array_to_avl_rec(int *array, avl_t *parent,
+                               size_t size, size_t start,
+                               size_t end)
+{
+	avl_t *head;
+	size_t h_idx;
+
+	if (start >= size / 2)
+	{
+		/** right **/
+		if (end == start)
+		{
+			head = new_node(array[end], parent);
+			if (end + 1 == size )
+			{
+				parent->right = parent->left;
+				parent->left = NULL;
+			}
+			return head;
+		} else if (end == size / 2)
+		{
+			return (NULL);
+		}
+	}
+
+	if (end <= size / 2)
+	{
+		/** left **/
+		if (start == end)
+			return new_node(array[start], parent);
+		else if (end < start)
+			return (NULL);
+	}
+
+	h_idx = (end - start) / 2;
+
+	if (start > size / 2)
+		h_idx += start;
+
+	head = new_node(array[h_idx], parent);
+
+	head->left = sorted_array_to_avl_rec(array, head, size, start, h_idx - 1);
+	head->right = sorted_array_to_avl_rec(array, head, size, h_idx + 1, end);
+	return (head);
+}
+
+static avl_t *new_node(int n, avl_t *parent)
+{
+	avl_t *node;
+
+	node = malloc(sizeof(avl_t));
+	if (!node)
+		return (NULL);
+	node->n = n;
+	node->parent = parent;
+	node->left = node->right = NULL;
+	if (parent)
+	{
+		if (!parent->left)
+			parent->left = node;
+		else if (!parent->right)
+			parent->right = node;
+	}
+	return (node);
+}
---
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -0,0 +1,34 @@
+#ifndef INTERVIEW_BINARY_TREES_H
+#define INTERVIEW_BINARY_TREES_H
+
+#include <stdio.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+typedef struct binary_tree_s avl_t;
+
+void from_avl_lib();
+void print_array(const int *array, size_t size);
+void binary_tree_print(const binary_tree_t *);
+char **binary_tree_string(const binary_tree_t *tree);
+
+avl_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *sorted_array_to_avl_rec(int *array, avl_t *parent, size_t size, size_t start, size_t end);
+
+#endif
