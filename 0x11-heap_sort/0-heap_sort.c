c7dca749a0b4893b77a76a8131a86dbcee9fec71
Martin Smith <mcodesmith@gmail.com>
2020-06-17 23:21:13 -0700

    add project file

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * heap_sort - performs min heap sort
+ * @array: array to sort
+ * @size: size of array
+ */
+void heap_sort(int *array, size_t size)
+{
+	print_array(array);
+}
