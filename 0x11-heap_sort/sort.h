e983b82f4ad05f0f0adb73eaaa339d43397c5d7a
Martin Smith <mcodesmith@gmail.com>
2020-06-24 16:37:03 -0700

    add project files to heap sort

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 
 void heap_sort(int *array, size_t size);
+
 void print_array(const int *array, size_t size);
+
+void heapify(int *arr, size_t arr_size, int h_size, int idx);
+void _swap(int *a, int *b);
 
 #endif
